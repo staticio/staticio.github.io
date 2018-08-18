e0eeb74665fedd46865752e7fa411d3aa1531117
Hoang Thieu <liberoht@gmail.com>
2018-08-18 12:59:21 +0700

    all chapter

--- static/user-scripts/site-scripts/toolz.truyencv.js
+++ static/user-scripts/site-scripts/toolz.truyencv.js
@@ -5,4 +5,6 @@ var chaptersLink = $("a[aria-controls='truyencv-detail-chap']").get(0);
 
 if(chaptersLink!=null){
     chaptersLink.click();
+    var allChapters = $(".panel-body nav ul:first-child li a").get(0);
+    allChapters.click();
 }
