7662b09f8ce47239230db10725b90dd351100de9
Hoang Thieu <liberoht@gmail.com>
2018-08-18 13:09:50 +0700

    fix

--- static/user-scripts/site-scripts/toolz.truyencv.js
+++ static/user-scripts/site-scripts/toolz.truyencv.js
@@ -5,7 +5,8 @@ var chaptersLink = $("a[aria-controls='truyencv-detail-chap']").get(0);
 
 if(chaptersLink!=null){
     console.log("truyencv: Show all chapters");
-    chaptersLink.click();
-    var allChapters = $(".panel-body nav ul:first-child li a").get(0);
-    allChapters.click();
+    chaptersLink.click(function(){
+        var allChapters = $(".panel-body nav ul:first-child li a").get(0);
+        allChapters.click();
+    });
 }
